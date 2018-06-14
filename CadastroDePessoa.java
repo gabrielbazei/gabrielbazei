44ab4748adef2bda827b6b665e4b97624c939f83
Gabriel de lima bazei <gabriel.lima.bazei@hotmail.com>
2018-06-14 13:00:50 -0300

    Add files via upload

---
+++ CadastroDePessoa.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exerciciosprog2;
+import java.util.Scanner;
+
+class Pessoa{
+    public String nome;
+    public float altura;
+    public float peso;
+}
+/**
+ *
+ * @author bazei
+ */
+public class CadastroDePessoa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner t=new Scanner(System.in);
+        Pessoa ser[]=new Pessoa[2];
+        ser[0]=new Pessoa();
+        ser[1]=new Pessoa();
+        for (int f=0;f<ser.length;f++){
+            System.out.println("Digite o nome da "+(f+1)+" pessoa: ");
+            ser[f].nome=t.nextLine();
+            System.out.println("Digite o peso do(a) "+ser[f].nome);
+            ser[f].peso=t.nextFloat();
+            System.out.println("Digite o altura do(a) "+ser[f].nome);
+            ser[f].altura=t.nextFloat();
+            t.nextLine();
+        }
+        if(ser[0].altura>=ser[1].altura){
+            System.out.println("O "+ser[0].nome+" e mais alto");
+        } else {
+            System.out.println("O "+ser[1].nome+" e mais alto");
+        }
+        if(ser[0].peso>=ser[1].peso){
+            System.out.println("O "+ser[0].nome+" e mais pesado");
+        } else {
+            System.out.println("O "+ser[1].nome+" e mais pesado");
+        }
+    }
+    
+}
